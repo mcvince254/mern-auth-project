1e0e2fdb0c0fe9d3ac85526fd615f272f114b28d
mcvince254 <mcvincemuthoni@gmail.com>
2026-04-06 22:29:01 +0300

    Update CORS and backend config

--- server/server.js
+++ server/server.js
@@ -22,13 +22,23 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.CLIENT_URL,
+];
+
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
-
 /* -------------------------------
    Health Check
 -------------------------------- */
