9669ee32bfd27a133f88fdbc67924bcbec8863c7
mcvince254 <mcvincemuthoni@gmail.com>
2026-08-20 11:27:38 +0300

    Update Home page

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,10 +1,14 @@
 import React from "react"
 import Navbar from "../components/Navbar"
 import Header from "../components/Header"
+import bgImg from "../assets/bg_img.png";
 
 const Home =()=>{
 return(
-    <div className="flex flex-col items-center justify-center min-h-screen bg-[url('bg_img.png')] bg-cover bg-center ">
+            <div
+            className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center"
+            style={{ backgroundImage: `url(${bgImg})` }}
+        >
         <Navbar/>
         <Header/>
        
