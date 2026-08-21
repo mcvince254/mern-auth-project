976c60884c1a68577ae15d8b3b7f335b3e9d6a13
mcvince254 <mcvincemuthoni@gmail.com>
2026-08-21 19:42:09 +0300

    Change Nodemailer configuration to use port 587-debuggin sendmail otp

--- server/config/nodemailer.js
+++ server/config/nodemailer.js
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.SMTP_EMAIL,
         pass: process.env.SMTP_PASSWORD
